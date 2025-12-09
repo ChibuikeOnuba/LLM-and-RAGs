39e655383190a7e43f3f69e72cf55cdc68c7eb19
ChibuikeOnuba <onubawinner042@gmail.com>
2025-12-09 12:58:42 +0100

    retrieval pipeline

---
+++ main retrieval.py
@@ -0,0 +1,36 @@
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+persistent_directory = "db/chroma_db"
+
+#Load embeddings and vector store
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+db = Chroma(
+    persist_directory=persistent_directory,
+    embedding_function=embedding_model,
+    collection_metadata={"hnsw:space":"cosine"}
+)
+
+#search through the relevant documents
+
+query = str(input())
+
+retriever = db.as_retriever(search_kwargs={"k":3})
+
+# retriever = db.as_retriever(search_type="similarity_score_threshold",
+#         search_kwargs={
+#             "k": 5,
+#             "score_threshold": 0.3}
+#             )
+
+
+relevant_docs = retriever.invoke(query)
+
+print(f"User Input: {query}")
+print("\n--------Context-------")
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"Document {i}:\n{doc.page_content}\n")
